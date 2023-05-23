34325e103b0cc599a5dbf542267ffc42c48dc9d5
alindaByamukama <alindabyamukama@gmail.com>
2023-05-23 23:02:40 +0300

    add various buttons with click events

--- my-app-dir/my-react-app/src/Content.js
+++ my-app-dir/my-react-app/src/Content.js
@@ -5,13 +5,28 @@ const Content = () => {
         const names = ['Bob', 'Kevin', 'Dave']
         const int = Math.floor(Math.random() * 3)
         return names[int]
-        }
+    }
+    
+  const handleClick = () => {
+      console.log('You clicked the button')
+  }
+  
+  const handleClick2 = (name) => {
+    console.log(`${name} was called`)
+  }
+
+  const handleClick3= (e) => {
+    console.log(e.target.innerText)
+  }
 
   return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
-        </p>
+      </p>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClick2('Dave')}>Click Me 2</button>
+      <button onClick={(e) => handleClick3(e)}>Click Me 3</button>
     </main>
   )
 }
