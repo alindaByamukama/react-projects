6569b3e5a82b896bc1d54f8595f707a5c0ea1e61
alindaByamukama <alindabyamukama@gmail.com>
2023-05-23 22:45:11 +0300

    add css style to index, remove inline styles from header, delete app css file and logo

--- my-app-dir/my-react-app/src/Header.js
+++ my-app-dir/my-react-app/src/Header.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
 const Header = () => {
-  const headerStyle = {
-    backgroundColor: 'royalblue',
-    color: '#ffff'
-  };
 
   return (
-    <header style={headerStyle}>
+    <header>
         <h1>Groceries List</h1>        
     </header>
   )
