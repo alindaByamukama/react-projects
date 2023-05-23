3fcba7a2d156898e374cafbe81f893af0e53a989
alindaByamukama <alindabyamukama@gmail.com>
2023-05-23 23:22:29 +0300

    add useEffect hooks to our content component

--- my-app-dir/my-react-app/src/Content.js
+++ my-app-dir/my-react-app/src/Content.js
@@ -1,32 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'Dave']
-        const int = Math.floor(Math.random() * 3)
-        return names[int]
-    }
+  const [name, setName] = useState('Dave')
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave']
+    const int = Math.floor(Math.random() * 3)
+    setName(names[int])
+  }
     
   const handleClick = () => {
-      console.log('You clicked the button')
+    setCount(count + 1)
+    setCount(count +1)
+    console.log(count)
   }
   
-  const handleClick2 = (name) => {
-    console.log(`${name} was called`)
-  }
-
-  const handleClick3= (e) => {
-    console.log(e.target.innerText)
+  const handleClick2 = () => {
+    console.log(count)
   }
 
   return (
     <main>
         <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}!
+            Hello {name}!
       </p>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={() => handleClick2('Dave')}>Click Me 2</button>
-      <button onClick={(e) => handleClick3(e)}>Click Me 3</button>
+      <button onClick={handleNameChange}>Chane Name</button>
+      <button onClick={handleClick}>Click Me 2</button>
+      <button onClick={handleClick2}>Click Me 3</button>
     </main>
   )
 }
