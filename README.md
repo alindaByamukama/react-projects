3d06f0389337d4dd841075993320b069db662a73
alindaByamukama <alindabyamukama@gmail.com>
2023-05-24 19:28:35 +0300

    add changes with controlled inputs - add & search items - notes in readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-projects
-# react-projects
+
+## Notes
+- Controlled Components
+  In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally. See: 
+  <a href='https://react.dev/learn/sharing-state-between-components#controlled-and-uncontrolled-components' target='_blank'>A single source of truth.</a>
--- my-app-dir/my-react-app/src/App.js
+++ my-app-dir/my-react-app/src/App.js
@@ -1,4 +1,6 @@
 import Header from './Header';
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
@@ -22,23 +24,53 @@ function App() {
     }
   ]);
 
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppingList', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
+
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log(newItem);
+    addItem(newItem);
+    setNewItem("");
   }
 
   return (
     <div className="App">
       <Header title="Groceries" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem 
+        search={search}
+        setSearch={setSearch}
+      />
       <Content 
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
